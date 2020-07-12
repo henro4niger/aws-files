728d531037d10da3dfc12213d493e4e1c39c7752
henro4niger <52746166+henro4niger@users.noreply.github.com>
2020-07-12 19:22:27 +0100

    Update lambda-upload-to-s3.py

--- lambda-upload-to-s3.py
+++ lambda-upload-to-s3.py
@@ -1,1 +1,10 @@
+import json
+import base64
+import boto3
 
+def lambda_handler(event, context):
+	s3 = boto3.client("s3")
+
+	get_file_content = event["content"]
+	decode_content = base64.b64decode(get_file_content)
+	s3_upload = s3.put_object( Bucket="upload-api-tutorial", Key="content.pdf", Body=decode_content)
